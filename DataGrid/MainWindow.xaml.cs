8a444f08756d6d82d2f94d601352194c0db19a28
komisarchuk <komisarchuk@gmail.com>
2019-10-08 21:00:20 +0300

    RadioButton

--- DataGrid/MainWindow.xaml.cs
+++ DataGrid/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using DataGrid.Entities;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,18 +28,41 @@ namespace DataGrid
     public partial class MainWindow : Window
     {
         private ObservableCollection<UserVM> _users = new ObservableCollection<UserVM>();
+        EFContext _context = new EFContext();
         public MainWindow()
         {
             InitializeComponent();
-            _users.Add(new UserVM()
-            {
-                Id = 1,
-                Name = "John Doe",
-                ImageUrl = "http://www.hawthorngroup.com/wp-content/uploads/2019/03/john-1-300x300.jpg"
-            });
-            dgSimple.ItemsSource = _users;
-        }
 
+            var _people = from p in _context.People
+                          select new
+                          {
+                              id = p.Id,
+                              name = p.Name,
+                              photo = p.Photo,
+                              sex = p.Sex
+                          };
+            dgSimple.ItemsSource = _people.ToList();
+
+            //_users.Add(new UserVM()
+            //{
+            //    Id = 1,
+            //    Name = "John Doe",
+            //    ImageUrl = "http://www.hawthorngroup.com/wp-content/uploads/2019/03/john-1-300x300.jpg"
+            //});
+            //dgSimple.ItemsSource = _users;
+        }
+        public class ObservableListSource<T> : ObservableCollection<T>, IListSource
+                    where T : class
+        {
+            private IBindingList _bindingList;
+
+            bool IListSource.ContainsListCollection { get { return false; } }
+
+            IList IListSource.GetList()
+            {
+                return _bindingList ?? (_bindingList = this.ToBindingList());
+            }
+        }
         private void BtnAddUser_Click(object sender, RoutedEventArgs e)
         {
             _users.Add(new UserVM() { Name = "New user" });
@@ -52,6 +79,11 @@ namespace DataGrid
                     userView.ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Katherine_Schwarzenegger_World_Dog_Awards_2015_%28cropped%29.jpg/653px-Katherine_Schwarzenegger_World_Dog_Awards_2015_%28cropped%29.jpg";
                 }
             }
+        }
+
+        private void DgSimple_Loaded(object sender, RoutedEventArgs e)
+        {
+           
         }
     }
 }
